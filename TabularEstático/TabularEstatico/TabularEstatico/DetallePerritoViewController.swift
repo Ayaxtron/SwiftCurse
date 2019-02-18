3041b4419556184ace9f8fc3a0be0c968f236cf5
Ayax <casarrubias2837@gmail.com>
2019-02-18 15:45:53 -0600

    Update

---
+++ TabularEstático/TabularEstatico/TabularEstatico/DetallePerritoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetallePerritoViewController.swift
+//  TabularEstatico
+//
+//  Created by Ayax Alexis Casarrubias Rodríguez on 2/18/19.
+//  Copyright © 2019 Ayax Alexis. All rights reserved.
+//
+
+import UIKit
+
+class DetallePerritoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
